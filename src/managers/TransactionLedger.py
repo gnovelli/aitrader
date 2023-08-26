aeff768f913ceeb43388af92c56e753e10295bf0
Giovanni Novelli, Ph.D <giovanni.novelli@gmail.com>
2023-08-26 21:21:16 +0200

    feat: Enhance TransactionLedger with detailed cash tracking
    - Extended TransactionLedger to provide a breakdown of cash balance.
    - Added tracking for total fees spent and total amount spent on cryptocurrencies.
    - Introduced a dictionary to monitor the quantity of each cryptocurrency owned.
    - Implemented a `get_cash_details()` method to retrieve a detailed financial overview.
    
    This update offers a more granular view of the user's financial activities and current cryptocurrency holdings.

--- src/managers/TransactionLedger.py
+++ src/managers/TransactionLedger.py
@@ -4,20 +4,26 @@ from models.Transaction import Transaction
 class TransactionLedger:
     def __init__(self, initial_cash_balance=0):
         self.transactions = []
-        self.cash_balance = initial_cash_balance  # Imposta il bilancio cash iniziale
-        self.crypto_balance = {}  # Un dizionario per tenere traccia delle diverse crypto
+        self.cash_balance = initial_cash_balance
+        self.total_fees = 0
+        self.total_spent_on_crypto = 0
+        self.crypto_balance = {}
 
     def add_transaction(self, transaction):
         if isinstance(transaction, Transaction):
             self.transactions.append(transaction)
 
-            # Aggiorna il bilancio cash
+            # Aggiorna il bilancio cash, le commissioni e l'importo speso in crypto
             if transaction.order_type in ["BUY", "LIMIT_BUY"]:
                 self.cash_balance -= (transaction.price * transaction.quantity + transaction.fee)
+                self.total_fees += transaction.fee
+                self.total_spent_on_crypto += (transaction.price * transaction.quantity)
                 self.crypto_balance[transaction.symbol] = self.crypto_balance.get(transaction.symbol,
                                                                                   0) + transaction.quantity
             elif transaction.order_type in ["SELL", "LIMIT_SELL"]:
                 self.cash_balance += (transaction.price * transaction.quantity - transaction.fee)
+                self.total_fees += transaction.fee
+                self.total_spent_on_crypto -= (transaction.price * transaction.quantity)
                 self.crypto_balance[transaction.symbol] = self.crypto_balance.get(transaction.symbol,
                                                                                   0) - transaction.quantity
 
@@ -44,6 +50,14 @@ class TransactionLedger:
             return self.crypto_balance.get(symbol, 0)
         return self.crypto_balance
 
+    def get_cash_details(self):
+        return {
+            "current_cash_balance": self.cash_balance,
+            "total_fees_spent": self.total_fees,
+            "total_spent_on_crypto": self.total_spent_on_crypto,
+            "crypto_balances": self.crypto_balance
+        }
+
 if __name__ == "__main__":
     # Inizializzazione del ledger con un bilancio cash di 10.000
     ledger = TransactionLedger(initial_cash_balance=1000000)
@@ -53,22 +67,35 @@ if __name__ == "__main__":
                                timestamp="2023-08-26 10:00:00", status="FILLED", fee=10, platform="Binance")
     ledger.add_transaction(transaction1)
     print("Dopo la transazione 1:")
-    print(f"Cash Balance: {ledger.get_cash_balance()}")
-    print(f"Crypto Balance (BTC): {ledger.get_crypto_balance('BTCUSDT')}\n")
+    details = ledger.get_cash_details()
+    print(f"Current Cash Balance: {details['current_cash_balance']}")
+    print(f"Total Fees Spent: {details['total_fees_spent']}")
+    print(f"Total Spent on Crypto: {details['total_spent_on_crypto']}")
+    for symbol, quantity in details['crypto_balances'].items():
+        print(f"Quantity of {symbol}: {quantity}")
 
     # Transazione 2: Acquisto di 2 ETH a 3000 ciascuno
     transaction2 = Transaction(transaction_id="T002", symbol="ETHUSDT", order_type="BUY", price=3000, quantity=2,
                                timestamp="2023-08-26 11:00:00", status="FILLED", fee=5, platform="Binance")
     ledger.add_transaction(transaction2)
     print("Dopo la transazione 2:")
-    print(f"Cash Balance: {ledger.get_cash_balance()}")
-    print(f"Crypto Balance (ETH): {ledger.get_crypto_balance('ETHUSDT')}\n")
+    details = ledger.get_cash_details()
+    print(f"Current Cash Balance: {details['current_cash_balance']}")
+    print(f"Total Fees Spent: {details['total_fees_spent']}")
+    print(f"Total Spent on Crypto: {details['total_spent_on_crypto']}")
+    for symbol, quantity in details['crypto_balances'].items():
+        print(f"Quantity of {symbol}: {quantity}")
 
     # Transazione 3: Vendita di 0.2 BTC a 52000 ciascuno
     transaction3 = Transaction(transaction_id="T003", symbol="BTCUSDT", order_type="SELL", price=52000, quantity=0.2,
                                timestamp="2023-08-26 12:00:00", status="FILLED", fee=8, platform="Binance")
     ledger.add_transaction(transaction3)
     print("Dopo la transazione 3:")
-    print(f"Cash Balance: {ledger.get_cash_balance()}")
-    print(f"Crypto Balance (BTC): {ledger.get_crypto_balance('BTCUSDT')}\n")
+    details = ledger.get_cash_details()
+    print(f"Current Cash Balance: {details['current_cash_balance']}")
+    print(f"Total Fees Spent: {details['total_fees_spent']}")
+    print(f"Total Spent on Crypto: {details['total_spent_on_crypto']}")
+    for symbol, quantity in details['crypto_balances'].items():
+        print(f"Quantity of {symbol}: {quantity}")
+
 
